8185c61c686abe8f98a148cbadffbace3616ad6d
varinkant123 <61323389+varinkant123@users.noreply.github.com>
2021-08-08 13:41:13 +1000

    Update app.R

--- app.R
+++ app.R
@@ -1,5 +1,8 @@
 library(shiny)
 
+options(shiny.sanitize.errors = FALSE)
+options(shiny.fullstacktrace = TRUE)
+
 ui <- fluidPage(
   
   fluidRow(column(4, selectInput(inputId = "I1", label = "Env Variable", choices = c("Yes", "No"), selected = "No")),
